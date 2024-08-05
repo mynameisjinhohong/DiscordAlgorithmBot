0a02ebd3ab7405046f041a805458441875bd310a
JinHo Hong <ghddhksduq@naver.com>
2024-08-05 12:02:09 +0900

    로그 추가

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ client.on('messageCreate', async message => {
                 return `<@${memberId}> 현재 벌금: ${fines[memberId]}원`;
             })
             .join('\n');
-
+            console.log(fines);
         generalChannel.send(allFinesMessages);
     }
 });
